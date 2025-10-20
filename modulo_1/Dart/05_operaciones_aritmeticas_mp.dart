8a7a14e6e85d70a04d45e094871c1c19184b5309
Kevin Cóndor <84334368+KevinEC17@users.noreply.github.com>
2025-10-19 21:11:57 -0500

    Commit Semana 3

---
+++ modulo_1/Dart/05_operaciones_aritmeticas_mp.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Operaciones Aritméticas");
+  final double costoVacuna = 12.5;
+  final double costoAlimento = 5;
+  print("Suma  $costoVacuna + $costoAlimento = ${costoVacuna + costoAlimento}");
+  print("Resta  $costoVacuna - $costoAlimento = ${costoVacuna - costoAlimento}");
+  print("Multiplicación  $costoVacuna * $costoAlimento = ${costoVacuna * costoAlimento}");
+  print("División  $costoVacuna / $costoAlimento = ${costoVacuna / costoAlimento}");
+}
