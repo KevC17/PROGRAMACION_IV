2a5d29a5a358ccc8c17cb1423b649e69f1d6366b
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-30 21:46:17 -0500

    Commit 30/10/25

---
+++ modulo_2/react_native/rn-purple-app/App.tsx
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return (
+    <><StatusBar style="light" /><RootNavigator /></>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
